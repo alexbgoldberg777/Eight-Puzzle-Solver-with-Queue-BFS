e724d61428bcdc7035ef668e9e30944c111a04d5
Alex Goldberg <98251459+alexbgoldberg777@users.noreply.github.com>
2022-01-24 17:28:25 -0500

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -3,13 +3,9 @@ package search;
 import java.util.List;
 
 /**
- * A solver for generic search problems.
- * The solver provides solutions based upon three search implementations.
- * 
- * @author liberato
- *
- * @param <T>
- *            the type of each state in the search problem
+ * This class is used to solve generic search problems and is imported to
+ * solve the Eight Puzzle game.
+ * @param <T> - the type of a state within the given search problem
  */
 public class Solver<T> {
   private final SearchProblem<T> problem;
@@ -19,38 +15,8 @@ public class Solver<T> {
   }
 
   /**
-   * Finds and returns a valid solution to the problem using recursive
-   * depth-first search.
-   * The solution will either be a list of states from the initial state to a
-   * goal state, or the empty list (if no solution was found).
-   * 
-   * @return a solution to the problem
-   */
-  public List<T> solveWithRecursiveDFS() {
-    Searcher<T> s = new RecursiveDepthFirstSearcher<T>(problem);
-    return s.findSolution();
-  }
-
-  /**
-   * Finds and returns a valid solution to the problem using iterative
-   * depth-first search.
-   * The solution will either be a list of states from the initial state to a
-   * goal state, or the empty list (if no solution was found).
-   * 
-   * @return a solution to the problem
-   */
-  public List<T> solveWithIterativeDFS() {
-    Searcher<T> s = new StackBasedDepthFirstSearcher<T>(problem);
-    return s.findSolution();
-  }
-
-  /**
-   * Finds and returns a valid solution to the problem using breadth-first
-   * search.
-   * The solution will either be a list of states from the initial state to a
-   * goal state, or the empty list (if no solution was found).
-   * 
-   * @return a solution to the problem
+   * @return a solution to a problem that searches with Breadth First
+   * Search in the form of a list of states.
    */
   public List<T> solveWithBFS() {
     Searcher<T> s = new QueueBasedBreadthFirstSearcher<T>(problem);
